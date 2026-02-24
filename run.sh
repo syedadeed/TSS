86a8f97cde0817217847f194079f44fa31b3bf6f
Syed Adeed <syedadeed@protonmail.com>
2026-02-24 13:39:18 +0530

    idk

--- run.sh
+++ run.sh
@@ -9,5 +9,5 @@ java -jar build/libs/tss-all.jar
 #   -e POSTGRES_DB=devdb \
 #   -p 5432:5432 \
 #   -v pgdata:/var/lib/postgresql \
-#   -v "$(pwd)/src/main/resources/db/migration/init_schema.sql:/docker-entrypoint-initdb.d/init_schema.sql:ro" \
+#   -v "$(pwd)/src/main/resources/db/migration/V1__init_schema.sql:/docker-entrypoint-initdb.d/init_schema.sql:ro" \
 #   -d postgres:latest

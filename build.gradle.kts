b311796202a6d5374c9ea53d02d943d4924f332d
Syed Adeed <syedadeed@protonmail.com>
2026-02-25 16:35:31 +0530

    assetj add

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,16 @@ dependencies{
     jooqCodegen(libs.jooq.postgres.extensions)
 
     compileOnly(libs.jakarta.xml.bind)
-    testCompileOnly(libs.jakarta.xml.bind)
 }
 
 testing{
     suites{
         val test by getting(JvmTestSuite::class){
             useJUnitJupiter("5.12.1")
+            dependencies{
+                implementation(libs.assertj.core)
+                compileOnly(libs.jakarta.xml.bind)
+            }
         }
     }
 }
---
+++ src/main/java/com/tss/Main.java
@@ -0,0 +1,7 @@
+package com.tss;
+
+class Main{
+    public static void main(String[] args){
+
+    }
+}
